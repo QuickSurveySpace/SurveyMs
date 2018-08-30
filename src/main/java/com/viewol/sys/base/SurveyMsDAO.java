aeff99c7572c094b75c95268f1723ff929419674
shileishmily <shileibrave@163.com>
2018-08-30 13:28:22 +0800

    first commit

---
+++ src/main/java/com/viewol/sys/base/SurveyMsDAO.java
@@ -0,0 +1,20 @@
+package com.viewol.sys.base;
+
+import com.youguu.core.dao.SqlDAO;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import javax.annotation.Resource;
+
+public class SurveyMsDAO<T> extends SqlDAO<T> {
+	public SurveyMsDAO() {
+		super();
+		setUseSimpleName(true);
+	}
+
+	@Resource(name = "surveyMsSessionFactory")
+	@Override
+	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+		super.setSqlSessionFactory(sqlSessionFactory);
+	}
+
+}
